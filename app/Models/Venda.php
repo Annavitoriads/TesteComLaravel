c7a20a0ef86db8b155dda41c1ae5d13e3fa08e6c
Vivis <annavitoriavianads@gmail.com>
2025-07-30 11:02:42 -0300

    feat: Implement basic CRUD operations for Clientes, Produtos, Vendas, Itens, and Parcelas

---
+++ app/Models/Venda.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Venda extends Model
+{
+    use HasFactory;
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function itens()
+    {
+        return $this->hasMany(ItemVenda::class);
+    }
+
+    public function parcelas()
+    {
+        return $this->hasMany(Parcela::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_07_30_123629_create_parcelas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parcelas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('venda_id')->constrained('vendas')->onDelete('cascade');
+            $table->decimal('valor', 10, 2);
+            $table->date('data_vencimento');
+            $table->boolean('paga')->default(false);
+            $table->timestamps();
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parcelas');
+    }
+};
